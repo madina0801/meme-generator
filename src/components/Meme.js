285b6227adad525d2dfacbc15a95ca52d7b66a73
Madina <madinatussupova0820@gmail.com>
2023-03-16 11:07:23 +0600

    add memes data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,21 @@
 import React from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
+	let url;
+
+	function getMemeImage(e) {
+		e.preventDefault();
+		let randomNum = Math.trunc(Math.random() * memesData.length);
+		url = memesData.data.memes[randomNum].url
+		console.log(url);
+	}
 	return (
 		<main>
 			<form className="form">
 				<input className="form__input" placeholder="Top text" />
 				<input className="form__input" placeholder="Bottom text" />
-				<button className="form__button">Get a new meme</button>
+				<button onClick={getMemeImage} className="form__button">Get a new meme</button>
 			</form>
 		</main>
 	)
