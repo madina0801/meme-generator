3bedbc39627dc4ea2f32f0199e4022cb84a6eadf
Madina <madinatussupova0820@gmail.com>
2023-03-17 13:36:15 +0600

    useState to get a new image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,25 @@
 import React from "react";
 import memesData from "../memesData.js";
 
-export default function Meme() {
-	let url;
+/**
+* Challenge: Save the random meme URL in state
+* - Create new state called `memeImage` with an
+*   empty string as default
+* - When the getMemeImage function is called, update
+*   the `memeImage` state to be the random chosen
+*   image URL
+* - Below the div.form, add an <img /> and set the
+*   src to the new `memeImage` state you created
+*/
 
+export default function Meme() {
+	const [memeUrl, setMemeUrl] = React.useState('');
+	
 	function getMemeImage(e) {
 		e.preventDefault();
-		let randomNum = Math.trunc(Math.random() * memesData.length);
-		url = memesData.data.memes[randomNum].url
-		console.log(url);
+		const memesArray = memesData.data.memes
+        const randomNum = Math.floor(Math.random() * memesArray.length)
+		setMemeUrl(memesData.data.memes[randomNum].url);
 	}
 	return (
 		<main>
@@ -17,6 +28,7 @@ export default function Meme() {
 				<input className="form__input" placeholder="Bottom text" />
 				<button onClick={getMemeImage} className="form__button">Get a new meme</button>
 			</form>
+			<img className="meme__img" src={memeUrl} />
 		</main>
 	)
 }
