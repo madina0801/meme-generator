81c1d11b00783033dbaa411be61a07a8fc3c6fea
Madina <madinatussupova0820@gmail.com>
2023-03-30 14:16:41 +0600

    get memes from api with useEffect

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React from "react";
-import memesData from "../memesData.js";
 
 export default function Meme() {
 	const [meme, setMeme] = React.useState({
@@ -8,13 +7,19 @@ export default function Meme() {
 		randomImage: "http://i.imgflip.com/1bij.jpg"
 	});
 
-	const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+	const [allMemes, setAllMemes] = React.useState([]);
+
+	React.useEffect(() => {
+		fetch('https://api.imgflip.com/get_memes')
+			.then(res => res.json())
+			.then(data => setAllMemes(data.data.memes))
+	}, []);
+	console.log(allMemes);
 
 	function getMemeImage(e) {
 		e.preventDefault();
-		const memesArray = allMemeImages.data.memes;
-		const randomNum = Math.floor(Math.random() * memesArray.length);
-		const url = memesData.data.memes[randomNum].url;
+		const randomNum = Math.floor(Math.random() * allMemes.length);
+		const url = allMemes[randomNum].url;
 		setMeme(prevMeme => {
 			return {
 				...prevMeme,
